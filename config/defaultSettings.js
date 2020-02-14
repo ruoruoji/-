e9aa41b82c419c0fb3ad31afa1b97b633561954c
sq <867149076@qq.com>
2020-02-15 00:12:14 +0800

    feat: 基于antd pro初始化项目

---
+++ config/defaultSettings.js
@@ -0,0 +1,18 @@
+export default {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: 'daybreak',
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    disableLocal: true,
+    // locale: true,
+  },
+  title: '科研信息管理平台',
+  pwa: false,
+  iconfontUrl: '',
+};
---
+++ src/components/GlobalHeader/RightContent.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { connect } from 'dva';
+import Avatar from './AvatarDropdown';
+import HeaderSearch from '../HeaderSearch';
+import styles from './index.less';
+import NoticeIconView from './NoticeIconView';
+
+const GlobalHeaderRight = props => {
+  const { theme, layout } = props;
+  let className = styles.right;
+
+  if (theme === 'dark' && layout === 'topmenu') {
+    className = `${styles.right}  ${styles.dark}`;
+  }
+
+  return (
+    <div className={className}>
+      <HeaderSearch
+        className={`${styles.action} ${styles.search}`}
+        placeholder="站内搜索"
+        defaultValue="umi ui"
+        options={[
+          {
+            label: <a href="https://umijs.org/zh/guide/umi-ui.html">umi ui</a>,
+            value: 'umi ui',
+          },
+          {
+            label: <a href="next.ant.design">Ant Design</a>,
+            value: 'Ant Design',
+          },
+          {
+            label: <a href="https://protable.ant.design/">Pro Table</a>,
+            value: 'Pro Table',
+          },
+          {
+            label: <a href="https://prolayout.ant.design/">Pro Layout</a>,
+            value: 'Pro Layout',
+          },
+        ]}
+      />
+      <NoticeIconView />
+      <Avatar menu />
+    </div>
+  );
+};
+
+export default connect(({ settings }) => ({
+  theme: settings.navTheme,
+  layout: settings.layout,
+}))(GlobalHeaderRight);
